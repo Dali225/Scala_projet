2db9601d1654691de42c949dc50c50071f11196d
sydali <aboubakardalitraore@gmail.com>
2022-10-27 10:45:45 +0200

    creer objet scala

--- src/main/scala/tondeuse.scala
+++ src/main/scala/tondeuse.scala
@@ -1,13 +1,11 @@
 
-class tondeuse ( coordonnees : (Int, Int) ){
-  def point : (x:Int )
-
+class tondeuse ( val x : Int, val y : Int, val dir : String ){
+ def X : Int = x
+ def Y : Int = y
 }
 
 objet tondeuses extend apps{
-  val tondeuse1 = new tondeuse(coordonnees = (5,5) )
-  //val tondeuse2 = new tondeuse(coordonnees = )
+val tondeuse1 = new tondeuse(x = 5, y = 5, dir = N )
+//val tondeuse2 = new tondeuse(coordonnees = )
 
-  println("La position de la tondeuse 1 est :" )
-
-}
+println("La position de la tondeuse 1 est :" )
